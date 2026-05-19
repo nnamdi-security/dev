70f5f0975e9d5b172791274d8c0e4ec83e41cd5e
nnamdi-security <peter.chizobam@xultechng.com>
2026-05-19 01:16:17 +0100

    added a lot

---
+++ full-stack/Hackathon/index.js
@@ -0,0 +1,64 @@
+/*let x = 1;
+
+function hotFunction(){
+    return x + x;
+}
+
+for (let i = 0; i < 10000; i++){
+    hotFunction();
+}
+x = "I'm now a string";
+console.log(hotFunction());
+
+
+const students = [
+    {name: "Amara", score: 70},
+    {name: 'Ozioma', score: 75},
+    {name: 'Ebuka', score: 98},
+    {name: 'Oluoma', score: 46},
+    {name: 'Oluchi', score: 88},
+];
+
+function filterStudents(students){
+    return students.filter(students => (students.score >= 70));
+}
+const passedStdents = filterStudents(students);
+console.log(passedStdents);
+
+
+function filterProducts(products){
+    return products.filter(products => products.price >= 500);
+}
+
+const products = 
+[
+    {name: 'tomatoes', price: 450},
+    {name: 'potatoes', price: 520},
+    {name: 'bananas', price: 500},
+    {name: 'bounrvita', price: 1200},
+    {name: 'himalayan', price: 400},
+    {name: 'okotorigba', price: 230},
+];
+
+const toBuyList = filterProducts(products);
+console.log(toBuyList);
+
+
+async function fetchAndDisplay(url){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }catch(error){
+        console.log(`error occurred: ${error}`);
+
+        return null;
+    }
+    
+}
+*/
+for (let i = 0; i < 1030; i++){
+    console.log(i);
+}
+
+
